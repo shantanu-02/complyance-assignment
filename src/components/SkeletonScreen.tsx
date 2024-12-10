56719e05b843650524a402094b26092b61a264b8
shantanu pawar <shantanupawar101@gmail.com>
2024-12-11 01:33:51 +0530

    Fix: ESLint errors

--- src/components/SkeletonScreen.tsx
+++ src/components/SkeletonScreen.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SkeletonScreen = () => {
 
     const SkeletonDiv = () => (
-    <div className="h-60 bg-gray-200 p-4 animate-pulse flex flex-col justify-center items-center rounded-lg">
+    <div className="h-[20rem] w-[20rem] bg-gray-200 p-4 animate-pulse flex flex-col justify-center items-center rounded-lg">
         <div className="mb-4 bg-gray-300 w-20 h-20 rounded-full" />
         <div className="h-4 bg-gray-300 w-32 mb-2 rounded" />
         <div className="h-4 bg-gray-300 w-24 rounded" />
